ecea4cee5dc06c3a7f6e75c651f6cce8e9464ea9
AbujafarCh <357jafar@gmail.com>
2024-04-12 17:56:42 +0600

    estate details page creation done with responsiveness

--- src/components/EstateDetails/EstateDetails.jsx
+++ src/components/EstateDetails/EstateDetails.jsx
@@ -3,7 +3,7 @@
 
 const EstateDetails = () => {
     return (
-        <div className="mt-12">
+        <div className="sm:mt-12 mt-7">
             <div>
                 <h1 className="font-poppins mb-2 font-semibold text-xl md:text-3xl">Charming Townhouse by the Park</h1>
                 <div className="mb-3">
@@ -13,11 +13,11 @@ const EstateDetails = () => {
             <div className="">
                 <img src="https://i.ibb.co/XFsd2TN/single-family.jpg" className="w-full rounded-md" />
             </div>
-            <div className="mt-12">
-                <h2>Property Details</h2>
+            <div className="mt-12  max-w-2xl mx-auto">
+                <h2 className="text-xl font-semibold font-poppins text-[#2A9D8F]">Property Details:</h2>
                 <div className="overflow-x-auto">
-                    <table className="table">
-                        <tbody>
+                    <table className="table border border-[#cfcfcf96] p-2 mt-4">
+                        <tbody className="text-base font-mont font-medium">
                             <tr>
                                 <td>Price</td>
                                 <td>$1500</td>
@@ -26,20 +26,20 @@ const EstateDetails = () => {
                                 <td>Status</td>
                                 <td>for Rent</td>
                             </tr>
-                            <tr className="hover">
+                            <tr>
                                 <td>Area</td>
                                 <td>2500 sq ft</td>
                             </tr>
                             <tr>
-                                <td>Facilities</td>
-                                <td>
+                                <td colSpan='2'>
+                                    <p className="font-semibold">Facilities:</p>
                                     <li>Study Lounges</li>
                                     <li>Fitness Center</li>
                                     <li>Communal Kitchen</li>
                                 </td>
                             </tr>
                             <tr>
-                                <td className="" colSpan='2'><span>Description:</span> Experience the epitome of urban living in this sleek and contemporary apartment located in the vibrant heart of the city. With its stylish design and convenient amenities, this apartment offers the perfect blend of comfort and sophistication. Enjoy stunning city views and easy access to all the best restaurants, shops, and entertainment venues.</td>
+                                <td className="" colSpan='2'><span className="font-semibold">Description:</span> Experience the epitome of urban living in this sleek and contemporary apartment located in the vibrant heart of the city. With its stylish design and convenient amenities, this apartment offers the perfect blend of comfort and sophistication. Enjoy stunning city views and easy access to all the best restaurants, shops, and entertainment venues.</td>
                             </tr>
                         </tbody>
                     </table>
